760c817b11871a567c445dd21245debfb75556d0
simonTomSt <simontomst.github@gmail.com>
2024-10-29 22:00:26 +0100

    Add paper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { Button } from '@/lib/components/button';
 import { Chip } from '@/lib/components/chip';
+import { Paper } from '@/lib/components/paper';
 
 export default function Home() {
   return (
-    <>
+    <Paper>
       <Button color='primary'>Hello World</Button>
       <Button color='soft'>Hello World</Button>
       <Chip label='Some text' />
-    </>
+    </Paper>
   );
 }
---
+++ src/lib/components/paper.tsx
@@ -0,0 +1,10 @@
+import MUIPaper from '@mui/material/Paper';
+import { styled, css } from '@mui/material/styles';
+
+export const Paper = styled(MUIPaper)(
+  ({ theme }) => css`
+    border-radius: 2px;
+    border: 1px solid ${theme.palette.grey[400]};
+    box-shadow: ${theme.boxShadow.grey};
+  `
+);
